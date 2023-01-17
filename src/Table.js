e14d26e7f7fdc69e3c9d99ff4dd33637adc21a90
Luke Aitchison <laitchis@calpoly.edu>
2023-01-17 13:42:55 -0800

    DELETE

--- src/Table.js
+++ src/Table.js
@@ -7,6 +7,7 @@ function TableHeader()  {
         <tr>
           <th>Name</th>
           <th>Job</th>
+          <th>Remove</th>
         </tr>
       </thead>
     );
@@ -16,9 +17,12 @@ function TableHeader()  {
     const rows = props.characterData.map((row, index) => {
       return (
         <tr key={index}>
-          <td>{row.name}</td>         
-          <td>{row.job}</td>
-        </tr>
+  <td>{row.name}</td>
+  <td>{row.job}</td>
+  <td>
+    <button onClick={() => props.removeCharacter(index)}>Delete</button>
+  </td>
+</tr>
       );
      }
     );
@@ -29,14 +33,14 @@ function TableHeader()  {
      );
   }
 
-  function Table(props) {
+  function Table (props) {
     return (
       <table>
         <TableHeader />
-        <TableBody characterData={props.characterData} />
+        <TableBody characterData={props.characterData} removeCharacter={props.removeCharacter} />
       </table>
     );
-}
+  }
 
 
 export default Table;
